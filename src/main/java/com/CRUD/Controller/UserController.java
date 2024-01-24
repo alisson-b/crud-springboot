2f34232e98f0ac98fbf5403db6f3ecd78f3b02b2
alisson-b <alisson.braza@gmail.com>
2024-01-24 13:59:14 -0300

    Add dos métodos get e post. Troca da server.port.

--- src/main/java/com/CRUD/Controller/UserController.java
+++ src/main/java/com/CRUD/Controller/UserController.java
@@ -4,9 +4,7 @@ import com.CRUD.Entity.User;
 import com.CRUD.Repository.UserRepository;
 import com.CRUD.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,9 +22,9 @@ public class UserController {
         return userRepository.findAll();
     }
 
-    @RequestMapping("/hello")
-    String home() {
-        return "Hello World!";
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user){
+        return userRepository.save(user);
     }
 
 
