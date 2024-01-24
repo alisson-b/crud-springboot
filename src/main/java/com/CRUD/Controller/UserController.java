ae3ec3162e2698788461c87541b41b425317a1dc
alisson-b <alisson.braza@gmail.com>
2024-01-24 17:14:23 -0300

    Add do método delete.

--- src/main/java/com/CRUD/Controller/UserController.java
+++ src/main/java/com/CRUD/Controller/UserController.java
@@ -4,6 +4,7 @@ import com.CRUD.Entity.User;
 import com.CRUD.Repository.UserRepository;
 import com.CRUD.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,11 +23,18 @@ public class UserController {
     public List<User> findAll() {
         return userRepository.findAll();
     }
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        return userRepository.findById(id)
+                .map(usuario -> ResponseEntity.ok().body(usuario))
+                .orElse(ResponseEntity.notFound().build());
+    }
 
     @PostMapping("/users")
     public User createUser(@RequestBody User user){
         return userRepository.save(user);
     }
+
     @PutMapping("users/{id}")
     public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable("id") Long id) {
         return userRepository.findById(id)
@@ -37,6 +45,14 @@ public class UserController {
                     User updated = userRepository.save(usuario);
                     return ResponseEntity.ok().body(updated);
                 }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id){
+        return userRepository.findById(id).map( usuario -> {
+                    userRepository.deleteById(id);
+                    return ResponseEntity.ok().build();
+         }).orElse(ResponseEntity.notFound().build());
     }
 
 
